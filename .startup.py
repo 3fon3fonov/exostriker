7d35152b02225157ba8ed17492a2da6a00119b40
3fon3fonov <44244057+3fon3fonov@users.noreply.github.com>
2018-10-26 18:40:40 +0200

    Delete .startup.py
    Former-commit-id: 4ca4d2e34b937923e261faf9ad799bf7d00c694a

--- .startup.py
+++
@@ -1,7 +0,0 @@
-# Ned's .startup.py file
-print("(.startup.py)")
-import datetime, os, pprint, re, sys, time
-import numpy as np
-print("(imported datetime, os, pprint, re, sys, time)")
-
-pp = pprint.pprint
