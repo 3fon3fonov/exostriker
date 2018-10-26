8a2d3bb14a3ccce9f606135f481138e7c13dc326
Trifon <3fon3fonov@gmail.com>
2018-10-26 18:26:27 +0200

    First commit
    Former-commit-id: 4e4591ec56773e63af22e2deeb82fe792fcac690

---
+++ .startup.py
@@ -0,0 +1,7 @@
+# Ned's .startup.py file
+print("(.startup.py)")
+import datetime, os, pprint, re, sys, time
+import numpy as np
+print("(imported datetime, os, pprint, re, sys, time)")
+
+pp = pprint.pprint
---
+++ prior_functions.py
@@ -0,0 +1,17 @@
+from pylab import *
+import numpy as np
+
+'''Here you can define custom priors. Give each an id, then you will be able to choose it by passing the id to mcmc (prior=id), since mcmc calls the choose_prior function.
+   If you don't provide prior id for mcmc id=0, i. e. flatprior, will be chosen.
+'''
+
+def choose_prior(p,i):
+	if (i==0):
+	    return flatprior(p)
+	else:
+	    return 0.0
+
+def flatprior (p):
+    '''id=0'''
+    return 0.0
+    
