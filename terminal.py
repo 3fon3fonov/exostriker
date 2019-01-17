33029a8253c9de826aa497d6f9125c63f6acd33c
Trifon <3fon3fonov@gmail.com>
2019-01-17 23:47:11 +0100

    text to buttons now added (RV,transit,activity)
    Former-commit-id: ab7274f2db78adeb15023e0e13b977ecc848af0b [formerly 9bcfdfc556a527770da38ba0cea97a6bdd8e6943]
    Former-commit-id: 13a58e0d3be9941892b6feb3c24c3968bb8bffc5

---
+++ terminal.py
@@ -0,0 +1,42 @@
+import sys
+from PyQt5 import QtCore, QtWidgets
+import subprocess
+import os
+
+
+class EmbTerminal(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super(EmbTerminal, self).__init__(parent)
+        self.process = QtCore.QProcess(self)
+        self.terminal = QtWidgets.QWidget(self)
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addWidget(self.terminal)
+        # Works also with urxvt:
+        if subprocess.call(["which", 'urxvt'], stdout=open(os.devnull, 'wb')) == 1:
+            self.process.start('xterm',['-into', str(int(self.winId()))])
+        else:
+            self.process.start('urxvt',['-embed', str(int(self.winId()))])
+        #self.setFixedSize(450, 390)
+ 
+
+class mainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(mainWindow, self).__init__(parent)
+
+        central_widget = QtWidgets.QWidget()
+        lay = QtWidgets.QVBoxLayout(central_widget)
+        self.setCentralWidget(central_widget)
+
+        tab_widget = QtWidgets.QTabWidget()
+        lay.addWidget(tab_widget)
+
+        tab_widget.addTab(EmbTerminal(), "EmbTerminal")
+        tab_widget.addTab(QtWidgets.QTextEdit(), "QTextEdit")
+        tab_widget.addTab(QtWidgets.QMdiArea(), "QMdiArea")
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    main = mainWindow()
+    main.show()
+    sys.exit(app.exec_())
