3cdc4adbe9ba9d27ce0c2a53f2d58217fda85241
Trifon <3fon3fonov@gmail.com>
2019-01-30 22:47:31 +0100

    Data inspector added
    Former-commit-id: 7d62971c3a1b97e928761edb945773c5b4bad2da [formerly 35947b349512f6b8e101d90ecac9b13ac80f054c]
    Former-commit-id: e574d6bfee0f04038b3ad67c93385565c39f2325

--- lib/terminal.py
+++ lib/terminal.py
@@ -15,10 +15,13 @@ class EmbTerminal(QtWidgets.QWidget):
         if subprocess.call(["which", 'urxvt'], stdout=open(os.devnull, 'wb')) == 1:
             self.process.start('xterm',['-into', str(int(self.winId()))])
         else:
+            #self.process.start('xterm',['-into', str(int(self.winId()))])
             self.process.start('urxvt',['-embed', str(int(self.winId()))])
-        self.setFixedSize(450, 340)
- 
+        #self.setFixedSize(450, 340)
+        self.setGeometry(1,1, 495, 315) 
+        #self.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
 
+        
 class mainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(mainWindow, self).__init__(parent)
@@ -27,12 +30,14 @@ class mainWindow(QtWidgets.QMainWindow):
         lay = QtWidgets.QVBoxLayout(central_widget)
         self.setCentralWidget(central_widget)
 
-        tab_widget = QtWidgets.QTabWidget()
-        lay.addWidget(tab_widget)
+       # tab_widget = QtWidgets.QTabWidget()
+        lay.addWidget(EmbTerminal())
+        self.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        #self.setMinimumSize(450,340)
 
-        tab_widget.addTab(EmbTerminal(), "EmbTerminal")
-        tab_widget.addTab(QtWidgets.QTextEdit(), "QTextEdit")
-        tab_widget.addTab(QtWidgets.QMdiArea(), "QMdiArea")
+        #tab_widget.addTab(EmbTerminal(), "EmbTerminal")
+        #tab_widget.addTab(QtWidgets.QTextEdit(), "QTextEdit")
+       # tab_widget.addTab(QtWidgets.QMdiArea(), "QMdiArea")
 
 
 if __name__ == "__main__":
