3f77eda2c0d7cdee8c6d8e5b04606319fd19b36e
Trifon <3fon3fonov@gmail.com>
2019-02-08 09:14:43 +0100

    some fixes are done
    Former-commit-id: 22cc0cdee1d71b3877889edaaf667c398a7985e2 [formerly bbe35e1dc9e4b6105edf5e571f97c8021d8cdcc5]
    Former-commit-id: 713c44f6da6ca2c49f499d1b379159859e50fd03

--- gui.py
+++ gui.py
@@ -17,6 +17,7 @@ import pyqtgraph.console as pg_console
 import word_processor_es as text_editor_es
 import calculator as calc 
 import gls as gls 
+from worker import Worker #, WorkerSignals
 
 #import time
 
@@ -90,78 +91,7 @@ class print_info(QtWidgets.QMainWindow):
         self.widget.layout().addWidget(self.text)
         self.setCentralWidget(self.widget)
         
-     
-class WorkerSignals(QtCore.QObject):
-    '''
-    Defines the signals available from a running worker thread.
-    Supported signals are:
-
-    finished
-        No data
-
-    error
-        `tuple` (exctype, value, traceback.format_exc() )
-
-    result
-        `object` data returned from processing, anything
-
-    progress
-        `int` indicating % progress
-    '''
-    finished = QtCore.pyqtSignal()
-    error = QtCore.pyqtSignal(tuple)
-    result = QtCore.pyqtSignal(object)
-    progress = QtCore.pyqtSignal(int)
-
-
-class Worker(QtCore.QRunnable):
-    '''
-    Worker thread
-
-    Inherits from QRunnable to handler worker thread setup, signals and wrap-up.
-
-    :param callback: The function callback to run on this worker thread. Supplied args and 
-                     kwargs will be passed through to the runner.
-    :type callback: function
-    :param args: Arguments to pass to the callback function
-    :param kwargs: Keywords to pass to the callback function
-
-    '''
-
-    def __init__(self, fn, *args, **kwargs):
-        super(Worker, self).__init__()
-
-        # Store constructor arguments (re-used for processing)
-        self.fn = fn
-        self.args = args
-        self.kwargs = kwargs
-        self.signals = WorkerSignals()
-
-        # Add the callback to our kwargs
-        self.kwargs['progress_callback'] = self.signals.progress
-
-    @QtCore.pyqtSlot()
-    def run(self):
-        '''
-        Initialise the runner function with passed args, kwargs.
-        '''
-
-        # Retrieve args/kwargs here; and fire processing using them
-        try:
-            result = self.fn()
-        except:
-            traceback.print_exc()
-            exctype, value = sys.exc_info()[:2]
-            self.signals.error.emit((exctype, value, traceback.format_exc()))
-        else:
-            self.signals.result.emit(result)  # Return the result of the processing
-        finally:
-            self.signals.finished.emit()  # Done
-            
-    def stop(self):
-            self.signals.finished.emit()  # Done
-    
-
+      
 class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def update_labels(self):
