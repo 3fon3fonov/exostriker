ad295b96ec51dd0f5308e4a853616e4c4a0cb4ca
Trifon <3fon3fonov@gmail.com>
2019-03-27 10:13:57 +0100

    major restructure on the GP part
    Former-commit-id: ddcd4a3de3b926c6db7c6d20c020520feffad5ee [formerly 74c5e656786a4361d144b5dcf2e57ee591839039]
    Former-commit-id: 9527a8a7ff53c9ea46ad54a31c5fa1fb688c5b39

--- lib/RV_mod/rot_kernels.py
+++ lib/RV_mod/rot_kernels.py
@@ -10,15 +10,15 @@ from celerite import terms
 
 def choose_kernel(i, params):
 	if (i==0):
-	    #Kernel=DefaultRotationTerm(log_amp=np.log(params[0]),log_timescale=np.log(params[1]),log_period=np.log(params[2]),log_factor=np.log(params[3]))
- 	    Kernel=DefaultRotationTerm(log_amp=params[0],log_timescale=params[1],log_period=params[2],log_factor=params[3])
+	    Kernel=DefaultRotationTerm(log_amp=np.log(params[0]),log_timescale=np.log(params[1]),log_period=np.log(params[2]),log_factor=np.log(params[3]))
+ 	    #Kernel=DefaultRotationTerm(log_amp=params[0],log_timescale=params[1],log_period=params[2],log_factor=params[3])
        
 	if not (Kernel.verify_number_of_parameters(len(params))):
 	    raise Exception('Number of parameters not suitable for this kernel, this would lead to errors later on!!!')
 	return Kernel
 	    
 
-class RotationTerm(terms.Term):
+class RotationTerm2(terms.Term):
 
     ''' to make sure a desired kernel has as many parameters as provided, otherwise there will be problems '''
 
@@ -28,18 +28,20 @@ class RotationTerm(terms.Term):
         else:
             return False
 
-class DefaultRotationTerm(RotationTerm):
-    parameter_names = ("log_amp", "log_timescale", "log_period", "log_factor")
-    number_of_parameters=4 
+#class DefaultRotationTerm(RotationTerm):
     
-    def parameter_labels(self):
-        return ['A','t','P','f']
+class RotationTerm(terms.Term):
+    
+    parameter_names = ("log_amp", "log_timescale", "log_period", "log_factor")
+   # number_of_parameters=4 
+    
+   # def parameter_labels(self):
+   #     return ['A','t','P','f']
 
     def get_real_coefficients(self, params):
         log_amp, log_timescale, log_period, log_factor = params
         f = np.exp(log_factor)
         return (np.exp(log_amp) * (1.0 + f) / (2.0 + f), np.exp(-log_timescale))   
-
 
     def get_complex_coefficients(self, params):
         log_amp, log_timescale, log_period, log_factor = params
