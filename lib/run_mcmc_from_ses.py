30cce990fcd81dbf034ca8a8fe75e45e449bcae3
Trifon <3fon3fonov@gmail.com>
2019-11-07 14:47:21 +0100

    MCMC and NS now run correctly in the BG.
    Former-commit-id: 208123c84526763face2316c42ad54efd7f91070 [formerly a1df80cea0a5d76750bc797bf11d9dd0cfd75bf4] [formerly 536af51eeb7f681c8003fc7baa269c92632263c1 [formerly e4a6f57cece5b1564339eb4d5573d15d974dd699]]
    Former-commit-id: 0cae25083355b28745126c7bf5dbf2f9478fe9ef [formerly 86e164295529b90aff96f5d6cf3d350f1667614d]
    Former-commit-id: 8a0161e8b8db4bb0231a0a343b11565439689e98

--- lib/run_mcmc_from_ses.py
+++ lib/run_mcmc_from_ses.py
@@ -3,16 +3,11 @@
 @author: Trifon Trifonov
 """
 
-
 import sys,  os
 sys.path.insert(0, './lib')
 sys.path.append('./lib/RV_mod/') #RV_mod directory must be in your path
 import RV_mod as rv
-import gls as gls
-import numpy as np
-import glob
 import dill
-import os
  
 
 arguments = len(sys.argv) - 1
@@ -29,8 +24,7 @@ if arguments==3 and sys.argv[1] == '-ses' and os.path.exists(sys.argv[2]):
  
     target_name = sys.argv[3] 
 
-    fit.live_points_fact = 100
-    fit = rv.run_mcmc(fit, burning_ph=100, mcmc_ph=100, threads=4, output=False, fileoutput=True, save_means=False, save_mode=False, save_maxlnL=False)
+    fit = rv.run_mcmc(fit)
      
     file_ses = open(r"%s_out.ses"%target_name, 'wb')
     dill.dump(fit, file_ses)
--- lib/run_ns_from_ses.py
+++ lib/run_ns_from_ses.py
@@ -8,11 +8,7 @@ import sys,  os
 sys.path.insert(0, './lib')
 sys.path.append('./lib/RV_mod/') #RV_mod directory must be in your path
 import RV_mod as rv
-import gls as gls
-import numpy as np
-import glob
 import dill
-import os
  
  
 arguments = len(sys.argv) - 1
@@ -29,8 +25,7 @@ if arguments==3 and sys.argv[1] == '-ses' and os.path.exists(sys.argv[2]):
  
     target_name = sys.argv[3] 
  
-    fit.live_points_fact = 100
-    fit = rv.run_nestsamp(fit, threads=4, std_output=False, stop_crit = 0.01, Dynamic_nest = False, live_points = 400,fileoutput=False )
+    fit = rv.run_nestsamp(fit)
      
     file_ses = open(r"%s_out.ses"%target_name, 'wb')
     dill.dump(fit, file_ses)
