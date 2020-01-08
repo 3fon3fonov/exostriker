c73aa56660cc26ce0fe5f123fc88d7f94b60ee5e
Trifon <3fon3fonov@gmail.com>
2020-01-08 13:11:37 +0100

    first docs
    Former-commit-id: 4347769541505103c7cac8e9fc3693ae36db5ceb [formerly d3881d9eb93d08b7ab87e8b07123a2e17da5ae05] [formerly c90e0a299e27d1c6e8ab9e876984995479514953 [formerly 3fa47018d9ad69382345740b428ffb99cd534a11]]
    Former-commit-id: c5a65cfdd8dbe6c8dca5d0057b4c8539d5675cb0 [formerly 797ed31df5077b9e1b272794df571db7e42a07b7]
    Former-commit-id: f19f1784dd1d8ff198ff85d2f8dfeb6180e161d9

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -37,7 +38,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.githubpages', "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -89,7 +90,10 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+
+#html_theme = 'alabaster'
+
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -197,3 +201,4 @@ epub_exclude_files = ['search.html']
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
