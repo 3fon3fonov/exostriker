03f606674dbebd2f8d67712f2b9f3b19454f0b07
Trifon <3fon3fonov@gmail.com>
2020-04-10 22:41:18 +0200

    Fast and interactive transit data detrending added! (part 3, non-blocking GUI)

--- es_gui.py
+++ es_gui.py
@@ -6741,7 +6741,9 @@ If this does not help, please open a GitHub issue here:
         if len(fit.tra_data_sets[but_ind-1]) != 0:
             self.tra_data = dill.copy(fit.tra_data_sets_init[but_ind-1])
 #           self.DetrendWindow.show()
-            self.DetrendWindow.plot()
+            self.DetrendWindow.worker_detrend()
+
+#            self.DetrendWindow.plot()
         else:
             
             print("No data ",but_ind)
