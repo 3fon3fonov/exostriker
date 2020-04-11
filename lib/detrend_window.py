1bc6c87d8f713c7efbdb05e1a4c90656f2ab5140
Trifon <3fon3fonov@gmail.com>
2020-04-11 20:32:04 +0200

    GLS of detr. data and model added

--- lib/detrend_window.py
+++ lib/detrend_window.py
@@ -7,6 +7,7 @@ import os
 from print_info_window import print_info
 from worker import Worker
 from multiprocessing import cpu_count
+import gls as gls
 
 from wotan import flatten
 
@@ -80,41 +81,27 @@ class DetrendWindow(QtWidgets.QWidget, Ui_DetrendWindow):
         self.info_dialog = print_info(self)
 
 
+        self.ui.buttonGroup_plot2.buttonClicked.connect(self.replot)
+
         self.ui.buttonGroup_trendOptions.buttonClicked.connect(self.update_labels)
 
-#        self.ui.aboutToQuit.connect(self.ui.radio_remove_mean.setChecked(True))
 
 
-
-
-    def closeEvent(self, event):
-        ret = QtGui.QMessageBox.question(None, 'Close request', 'Are you sure you want to quit?',
-                                         QtGui.QMessageBox.Yes | QtGui.QMessageBox.No,
-                                         QtGui.QMessageBox.Yes)
-        if ret == QtGui.QMessageBox.Yes:
-            self.ui.radio_remove_mean.setChecked(True)
-            QtGui.QMainWindow.closeEvent(self, event)
-        else:
-            event.ignore()
-
-
-    def save_data_product(self):
-
-        output_file = QtGui.QFileDialog.getSaveFileName(self, 'Save detrended data file', 'detrended_%s'%self.data_file_name, 'All (*.*);;Data (*.tran)', options=QtGui.QFileDialog.DontUseNativeDialog)
+    def replot(self):
         
-        
-        if str(output_file[0]) != '':
+        if self.ui.GLS_of_data.isChecked():
+            self.make_GLS()
+            self.plot_GLS()
+        elif self.ui.GLS_of_model.isChecked():
+            self.make_GLS(model=True)
+            self.plot_GLS()
+        elif self.ui.GLS_of_detr_data.isChecked():
+            self.make_GLS(model=False,o_c=True)
+            self.plot_GLS()
+        elif self.ui.flatten_data.isChecked():
+            self.bottom_plot_lc()
+
  
-            f = open(output_file[0], 'w')
-
-            for i in range(len(self.t)):
-                f.write('{0:{width}.{precision}f}  {1:{width}.{precision}f}  {2:{width}.{precision}f}  \n'.format(float(self.t[i]), float(self.flux_o_c[i]), float(self.flux_err_o_c[i]),  width = 10, precision = 7 )   )
-                
-            f.close()
-
-
-
-
     def calculate(self):
 
         
@@ -161,7 +148,7 @@ class DetrendWindow(QtWidgets.QWidget, Ui_DetrendWindow):
                 break_tolerance=self.ui.poly_bt.value(),  # Split into segments at breaks longer than that
                 return_trend=True,    # Return trend and flattened light curve
                 )
-            
+
         elif self.ui.radio_Regressions.isChecked():
 
             flatten_lc1, trend_lc1 = flatten(
@@ -174,7 +161,6 @@ class DetrendWindow(QtWidgets.QWidget, Ui_DetrendWindow):
                 )
             
         elif self.ui.radio_GPs.isChecked():
-            
 
             flatten_lc1, trend_lc1 = flatten(
                 self.t,                 # Array of time values
@@ -185,33 +171,36 @@ class DetrendWindow(QtWidgets.QWidget, Ui_DetrendWindow):
                 break_tolerance=self.ui.regres_bt.value(),  # Split into segments at breaks longer than that
                 return_trend=True,    # Return trend and flattened light curve
                 )
-            
-            
+
         else:
             flatten_lc1 = self.flux 
             trend_lc1 = np.ones(len(self.flux))*np.mean(self.flux)
 
 
-            
         self.flux_o_c = flatten_lc1
         self.trend = trend_lc1
         self.flux_err_o_c = self.flux_err/trend_lc1
-        
         
 
 
 
     def worker_detrend_complete(self):
 
-        self.ui.label_working.setText("")
+        #self.ui.label_working.setText("")
+        self.ui.try_button.setText("Try !")
         self.ui.try_button.setEnabled(True)
+        self.ui.flatten_data.setChecked(True)
+
         self.plot()
+        self.show()
+
         return
         
     def worker_detrend(self):
 
         self.ui.try_button.setEnabled(False)
-        self.ui.label_working.setText("Working!!!")
+        #self.ui.label_working.setText("Working!!!")
+        self.ui.try_button.setText("Working!!!")
         worker_detrend_wk = Worker(self.calculate)# Any other args, kwargs are passed to the run  
         worker_detrend_wk.signals.finished.connect(self.worker_detrend_complete)
 
@@ -222,17 +211,20 @@ class DetrendWindow(QtWidgets.QWidget, Ui_DetrendWindow):
 
 
 
-
-
-
- 
     def plot(self):
 
-        #self.worker_detrend()
-        #self.ui.label_working.setText("")
+        self.top_plot()
+        self.bottom_plot_lc()
+
+
+    def top_plot(self):
+        global p_1
+        
+        p_1.plot(clear=True,)
+
         ######## Top plot ############
 
-        self.ui.plot.plot(self.t,self.flux, clear=True, pen=None,
+        p_1.plot(self.t,self.flux, pen=None,
             symbol='o', symbolPen={'color': '#0066ff', 'width': 1.1},
             symbolSize=2,enableAutoRange=True,viewRect=True,
             symbolBrush='#0066ff')
@@ -241,27 +233,105 @@ class DetrendWindow(QtWidgets.QWidget, Ui_DetrendWindow):
                                    top=self.flux_err, 
                                    bottom=self.flux_err,
                                    beam=0.0, pen='#0066ff')
-                                   
-        self.ui.plot.addItem(err_)
 
-        model_curve = self.ui.plot.plot(self.t, self.trend , pen={'color': '#000000', 'width': 3}, enableAutoRange=True,viewRect=True ) 
+        p_1.addItem(err_)
+
+        model_curve = p_1.plot(self.t, self.trend , pen={'color': '#000000', 'width': 3}, enableAutoRange=True,viewRect=True ) 
         model_curve.setZValue(1)
 
 
-        ######## Bottom plot ############
 
-        self.ui.plot_2.plot(self.t,self.flux_o_c, clear=True, pen=None,
+    def bottom_plot_lc(self):
+        global p_2
+        
+        p_2.plot(clear=True,)
+        ######## Bottom plot ############
+        p_2.setLogMode(False,False)
+
+        p_2.plot(self.t,self.flux_o_c, pen=None,
             symbol='o', symbolPen={'color': '#0066ff', 'width': 1.1},
             symbolSize=2,enableAutoRange=True,viewRect=True,
             symbolBrush='#0066ff')
+            
+        p_2.setLabel('left', 'Flux', units='',  **{'font-size':'9pt'})
+
 
         err_ = pg.ErrorBarItem(x=self.t, y=self.flux_o_c, symbol = 'o',
                                    top=self.flux_err_o_c, 
                                    bottom=self.flux_err_o_c,
                                    beam=0.0, pen='#0066ff')
                                    
-        self.ui.plot_2.addItem(err_)
-        self.show()
+        p_2.addItem(err_)
+        
+        
+        
+
+    def make_GLS(self, model=False, o_c=False):
+
+        #omega = 1/ np.logspace(np.log10(self.parent.gls_min_period.value()), np.log10(self.parent.gls_max_period.value()), num=int(self.parent.gls_n_omega.value()))
+        
+        omega = 1/ np.logspace(np.log10(0.9), np.log10((max(self.t)-min(self.t))*2.0), num=int(self.parent.gls_n_omega.value()))
+        
+        if model == False and o_c == False:
+            data_for_GLS   = self.flux
+            e_data_for_GLS = self.flux_err
+        elif model == False and  o_c == True:
+            data_for_GLS   = self.flux_o_c
+            e_data_for_GLS = self.flux_err_o_c
+        else:
+            data_for_GLS   = self.trend
+            e_data_for_GLS = self.flux_err
+
+        self.trend_per = gls.Gls((self.t, data_for_GLS, e_data_for_GLS), 
+            fast=True,  verbose=False, norm= "ZK",ofac=self.parent.gls_ofac.value(), fbeg=omega[-1], fend=omega[ 0],)
+
+
+
+    def plot_GLS(self):
+        global p_2
+
+        p_2.plot(clear=True,)
+
+        power_levels = np.array([self.parent.gls_fap1.value(),self.parent.gls_fap2.value(),self.parent.gls_fap3.value()])
+
+        ######################## GLS ##############################
+        if self.parent.radioButton_act_GLS_period.isChecked():
+            p_2.setLogMode(True,False)
+            p_2.plot(1.0/self.trend_per.freq, self.trend_per.power,pen='r',symbol=None ) 
+            p_2.setLabel('bottom', 'period [d]', units='',  **{'font-size':'9pt'}) 
+            p_2.setLabel('left', 'Power', units='',  **{'font-size':'9pt'})
+
+        else:
+            p_2.setLogMode(False,False)        
+            p_2.plot(self.trend_per.freq, self.trend_per.power,pen=f'r',symbol=None )
+            p_2.setLabel('bottom', 'frequency [1/d]', units='',  **{'font-size':'9pt'}) 
+
+        [p_2.addLine(x=None, y=fap, pen=pg.mkPen('k', width=0.8, style=QtCore.Qt.DotLine)) for ii,fap in enumerate(self.trend_per.powerLevel(np.array(power_levels)))]
+
+
+
+
+    def closeEvent(self, event):
+        ret = QtGui.QMessageBox.question(None, 'Close request', 'Are you sure you want to quit?',
+                                         QtGui.QMessageBox.Yes | QtGui.QMessageBox.No,
+                                         QtGui.QMessageBox.Yes)
+        if ret == QtGui.QMessageBox.Yes:
+            self.ui.radio_remove_mean.setChecked(True)
+            QtGui.QMainWindow.closeEvent(self, event)
+        else:
+            event.ignore()
+
+
+    def save_data_product(self):
+
+        output_file = QtGui.QFileDialog.getSaveFileName(self, 'Save detrended data file', 'detrended_%s'%self.data_file_name, 'All (*.*);;Data (*.tran)', options=QtGui.QFileDialog.DontUseNativeDialog)
+        
+        if str(output_file[0]) != '':
+            f = open(output_file[0], 'w')
+            for i in range(len(self.t)):
+                f.write('{0:{width}.{precision}f}  {1:{width}.{precision}f}  {2:{width}.{precision}f}  \n'.format(float(self.t[i]), float(self.flux_o_c[i]), float(self.flux_err_o_c[i]),  width = 10, precision = 7 )   )
+            f.close()
+
 
 
     def info(self):
@@ -362,33 +432,39 @@ dependencies list. For example:
 
     def initialize_plots(self):
 
+        global p_1,p_2
+        
         xaxis = ['BJD [days]','BJD [days]']
         yaxis = ['Flux','Flux']
         xunit = ['' ,'']
         yunit = ['' ,'' ]
 
-        zzz = [self.ui.plot,self.ui.plot_2]
+        p_1 = self.ui.plot
+        p_2 = self.ui.plot_2
+
+        zzz = [p_1,p_2]
+
 
         for i in range(len(zzz)):
 
-                zzz[i].getAxis("bottom").tickFont = self.font
-                zzz[i].getAxis("bottom").setStyle(tickTextOffset = 12)
-                zzz[i].getAxis("top").tickFont = self.font
-                zzz[i].getAxis("top").setStyle(tickTextOffset = 12)
-                zzz[i].getAxis("left").tickFont = self.font
-                zzz[i].getAxis("left").setStyle(tickTextOffset = 12)
-                zzz[i].getAxis("right").tickFont = self.font
-                zzz[i].getAxis("right").setStyle(tickTextOffset = 12)
-                zzz[i].getAxis('left').setWidth(50)
-                zzz[i].getAxis('right').setWidth(10)
-                zzz[i].getAxis('top').setHeight(10)
-                zzz[i].getAxis('bottom').setHeight(50)
-                            
-                zzz[i].setLabel('bottom', '%s'%xaxis[i], units='%s'%xunit[i],  **{'font-size':'9pt'})
-                zzz[i].setLabel('left',   '%s'%yaxis[i], units='%s'%yunit[i],  **{'font-size':'9pt'})       
-                zzz[i].showAxis('top') 
-                zzz[i].showAxis('right') 
-                zzz[i].getAxis('bottom').enableAutoSIPrefix(enable=False)
+            zzz[i].getAxis("bottom").tickFont = self.font
+            zzz[i].getAxis("bottom").setStyle(tickTextOffset = 12)
+            zzz[i].getAxis("top").tickFont = self.font
+            zzz[i].getAxis("top").setStyle(tickTextOffset = 12)
+            zzz[i].getAxis("left").tickFont = self.font
+            zzz[i].getAxis("left").setStyle(tickTextOffset = 12)
+            zzz[i].getAxis("right").tickFont = self.font
+            zzz[i].getAxis("right").setStyle(tickTextOffset = 12)
+            zzz[i].getAxis('left').setWidth(50)
+            zzz[i].getAxis('right').setWidth(10)
+            zzz[i].getAxis('top').setHeight(10)
+            zzz[i].getAxis('bottom').setHeight(50)
+
+            zzz[i].setLabel('bottom', '%s'%xaxis[i], units='%s'%xunit[i],  **{'font-size':'9pt'})
+            zzz[i].setLabel('left',   '%s'%yaxis[i], units='%s'%yunit[i],  **{'font-size':'9pt'})       
+            zzz[i].showAxis('top') 
+            zzz[i].showAxis('right') 
+            zzz[i].getAxis('bottom').enableAutoSIPrefix(enable=False)
 
         #zzz[i].getViewBox().setAspectLocked(True)
 
