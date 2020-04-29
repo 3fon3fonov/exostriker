3ccd868987b5b447b694084cac0a5d9596d30673
Trifon <3fon3fonov@gmail.com>
2020-04-29 15:54:44 +0200

    More precision added in the GUI front-end

--- lib/terminal.py
+++ lib/terminal.py
@@ -7,10 +7,16 @@ import os
 class EmbTerminal(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(EmbTerminal, self).__init__(parent)
-        self.process = QtCore.QProcess(self)
-        #self.terminal = QtWidgets.QWidget(self)
-        layout = QtWidgets.QVBoxLayout(self)
 
+        layout = QtWidgets.QGridLayout()
+       # layout.setSpacing(0)  # for simplicity
+       # layout.setSizeConstraint(QtWidgets.QLayout.SetNoConstraint)
+
+        self.setLayout(layout)
+        
+        self.process = QtCore.QProcess(self)
+        self.terminal = QtWidgets.QWidget(self)
+        
         
         # Works also with urxvt:
         if subprocess.call(["which", 'urxvt'], stdout=open(os.devnull, 'wb')) == 1:
@@ -18,36 +24,62 @@ class EmbTerminal(QtWidgets.QWidget):
         else:
             #self.process.start('xterm',['-into', str(int(self.winId()))])
             self.process.start('urxvt',['-embed', str(int(self.winId()))])
+            
+        #self.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+
         #self.setFixedSize(450, 340)
-        self.setGeometry(1,1, 495, 325) 
-        #self.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-        #layout.addWidget(self) 
-        self.setLayout(layout)
-        #self.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        self.setGeometry(1,1, 495, 390) 
+        #self.terminal.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+   # def sizeHint(self):
+   #     size = super(EmbTerminal, self).sizeHint()
+   #     size.setHeight(size.height() + 30)
+   #     size.setWidth(max(size.width(), size.height()))
+   #     return size
+    
+    def someFunction(self):
+        self.terminal.setGeometry(1, 1, self.width(), self.height())
         
+        print(self.terminal.width(), self.terminal.height(),self.width(), self.height())
+        #print("someFunction")
+        #return super(EmbTerminal, self).__init__()
+        #self.setFixedSize(self.width(), self.height())
+
     def close(self):
         self.process.kill()
         
 class mainWindow(QtWidgets.QMainWindow):
+    
+    resized = QtCore.pyqtSignal()
+    
     def __init__(self, parent=None):
         super(mainWindow, self).__init__(parent)
 
-        central_widget = QtWidgets.QWidget()
-        lay = QtWidgets.QVBoxLayout(central_widget)
-        self.setCentralWidget(central_widget)
 
-       # tab_widget = QtWidgets.QTabWidget()
-        lay.addWidget(EmbTerminal())
-        self.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-        #self.setMinimumSize(450,340)
 
-        #tab_widget.addTab(EmbTerminal(), "EmbTerminal")
-        #tab_widget.addTab(QtWidgets.QTextEdit(), "QTextEdit")
-       # tab_widget.addTab(QtWidgets.QMdiArea(), "QMdiArea")
+        layout = QtWidgets.QGridLayout()
+        self.term = EmbTerminal()
+        layout.addWidget(self.term)
 
+        container = QtWidgets.QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+ 
+        #self.resized.connect(self.term.someFunction)
+
+    def resizeEvent(self, event):
+        self.resized.emit()
+        return super(mainWindow, self).resizeEvent(event)
+
+
+
+    def close(self):
+        self.term.process.kill()
 
 if __name__ == "__main__":
+    
     app = QtWidgets.QApplication(sys.argv)
-    main = EmbTerminal()
+    main = mainWindow()
     main.show()
     sys.exit(app.exec_())
+ 
