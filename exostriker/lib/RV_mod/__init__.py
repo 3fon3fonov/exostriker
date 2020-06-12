some fixes in M0 calculation + TTVs

exostriker/lib/RV_mod/__init__.py
@@ -376,9 +376,9 @@ def ttvs_mod(par,vel_files,npl, stellar_mass, times, planet_N, fit_results=False
                                             par[len(vel_files)*2 +7*i+5],
                                             par[len(vel_files)*2 +7*i+6],
                                             #par[len(vel_files)*2 +7*i+3]%360.0,
-                                            (par[len(vel_files)*2 +7*i+3]-180.0)%360.0,
+                                            (par[len(vel_files)*2 +7*i+3])%360.0,
                                             #par[len(vel_files)*2 +7*i+4]%360.0]
-                                            (par[len(vel_files)*2 +7*i+4]+180.0)%360.0]
+                                            (par[len(vel_files)*2 +7*i+4])%360.0]
 
         planet = ttvfast.models.Planet(*pl_params)
         planets.append(planet)
@@ -426,8 +426,8 @@ def ttvs_loglik(par,vel_files,ttv_files,npl,stellar_mass,times, fit_results = Fa
                                             par[len(vel_files)*2 +7*i+2],
                                             par[len(vel_files)*2 +7*i+5],
                                             par[len(vel_files)*2 +7*i+6],
-                                            (par[len(vel_files)*2 +7*i+3]-180.0)%360.0,
-                                            (par[len(vel_files)*2 +7*i+4]+180.0)%360.0]
+                                            (par[len(vel_files)*2 +7*i+3])%360.0,
+                                            (par[len(vel_files)*2 +7*i+4])%360.0]
         planet = ttvfast.models.Planet(*pl_params)
         planets.append(planet)
 
@@ -771,8 +771,9 @@ def model_loglik(p, program, par, flags, npl, vel_files, tr_files, tr_model, tr_
             else:
                 ecc_, om_, = par[len(vel_files)*2 +7*i+2],par[len(vel_files)*2 +7*i+3]
 
-            par[len(vel_files)*2 +7*i+4] = ma_from_t0(par[len(vel_files)*2 +7*i+1],
-                                                      ecc_, om_, par[len(vel_files)*2 +7*i+6], par[len(vel_files)*2 +7*npl + 2 +rv_gp_npar + 3*i],epoch)
+            #par[len(vel_files)*2 +7*i+4] = ma_from_t0(par[len(vel_files)*2 +7*i+1],
+            #                                          ecc_, om_, par[len(vel_files)*2 +7*i+6], par[len(vel_files)*2 +7*npl + 2 +rv_gp_npar + 3*i],epoch)
+            par[len(vel_files)*2 +7*i+4] = get_m0(par[len(vel_files)*2 +7*i+1], ecc_, om_)
  
     else:
         for i in range(npl):
@@ -784,7 +785,7 @@ def model_loglik(p, program, par, flags, npl, vel_files, tr_files, tr_model, tr_
                 ecc_, om_, Ma_ = par[len(vel_files)*2 +7*i+2], par[len(vel_files)*2 +7*i+3], par[len(vel_files)*2 +7*i+4]
 
             par[len(vel_files)*2 +7*npl + 2 +rv_gp_npar + 3*i] = transit_tperi(par[len(vel_files)*2 +7*i+1],
-                                                                  ecc_, om_,par[len(vel_files)*2 +7*i+6], Ma_ ,epoch)[1]%par[len(vel_files)*2 +7*i+1]
+                                                                  ecc_, om_, Ma_ ,epoch)[1]%par[len(vel_files)*2 +7*i+1]
 
     if(rtg[0]):
 
@@ -1182,8 +1183,10 @@ def return_results(obj, pp, ee, par,flags, npl,vel_files, tr_files, tr_model, tr
             else:
                 ecc_, om_, = par[len(vel_files)*2 +7*i+2],par[len(vel_files)*2 +7*i+3]
 
-            par[len(vel_files)*2 +7*i+4] = ma_from_t0(par[len(vel_files)*2 +7*i+1],
-                                                      ecc_, om_, par[len(vel_files)*2 +7*i+6], par[len(vel_files)*2 +7*npl + 2 +rv_gp_npar + 3*i],epoch)
+            #par[len(vel_files)*2 +7*i+4] = ma_from_t0(par[len(vel_files)*2 +7*i+1],
+           #                                           ecc_, om_, par[len(vel_files)*2 +7*i+6], par[len(vel_files)*2 +7*npl + 2 +rv_gp_npar + 3*i],epoch)
+            par[len(vel_files)*2 +7*i+4] = get_m0(par[len(vel_files)*2 +7*i+1], ecc_, om_)
+
             obj.params.update_M0(i,par[len(vel_files)*2 +7*i+4])
 
         obj.transit_results = transit_loglik(mod, tr_files,vel_files,tr_params,tr_model,par,rv_gp_npar,tra_gp_npar,obj.npl,obj.hkl, obj.rtg, obj.tra_gps,stmass, obj.ttv_times, obj.epoch, return_model = True, tra_model_fact=obj.tra_model_fact)
@@ -1212,8 +1215,9 @@ def return_results(obj, pp, ee, par,flags, npl,vel_files, tr_files, tr_model, tr
             else:
                 ecc_, om_, = par[len(vel_files)*2 +7*i+2],par[len(vel_files)*2 +7*i+3]
 
-            par[len(vel_files)*2 +7*i+4] = ma_from_t0(par[len(vel_files)*2 +7*i+1],
-                                                      ecc_, om_, par[len(vel_files)*2 +7*i+6], par[len(vel_files)*2 +7*npl + 2 +rv_gp_npar + 3*i],epoch)
+            #par[len(vel_files)*2 +7*i+4] = ma_from_t0(par[len(vel_files)*2 +7*i+1],
+            #                                          ecc_, om_, par[len(vel_files)*2 +7*i+6], par[len(vel_files)*2 +7*npl + 2 +rv_gp_npar + 3*i],epoch)
+            par[len(vel_files)*2 +7*i+4] = get_m0(par[len(vel_files)*2 +7*i+1], ecc_, om_)
 
             obj.params.update_M0(i,par[len(vel_files)*2 +7*i+4])
 
@@ -3940,12 +3944,15 @@ class signal_fit(object):
         #print(self.fit_results.stat.dof)
         self.hack_around_rv_params()
 
-
+ 
 
         ##################### New stuff to be added here ######################
         for i in range(self.npl):
-            self.t_peri[i] = (float(self.epoch) - (np.radians(self.M0[i])/(2*np.pi))*self.P[i] ) # epoch  - ((ma/TWOPI)*a[1])
-        #####################
+            #self.t_peri[i] = (float(self.epoch) - (np.radians(self.M0[i])/(2*np.pi))*self.P[i] ) # epoch  - ((ma/TWOPI)*a[1])
+ 
+            self.t_peri[i] = transit_tperi(self.P[i],self.e[i], self.w[i], self.M0[i] ,self.epoch)[0]
+
+       #####################
 
 
         if (return_flag):

exostriker/lib/RV_mod/functions.py
@@ -29,11 +29,9 @@ TAU= 2.0*np.pi
 
 
 
-
-def transit_tperi(per, ecc, om,Omega, ma, epoch):
+def transit_tperi(per, ecc, om, ma, epoch):
     """It derives Time of periatron [tp]
     and time of mid transit [t0]
-
     Parameters
     ----------
     per : float
@@ -46,35 +44,36 @@ def transit_tperi(per, ecc, om,Omega, ma, epoch):
         Mean anomaly [deg].
     epoch : float
         Epoch for wich the orbital elements are valid [BJD].
-
     Returns
     -------
     [tp,t0]
         if the epoch in is BJD then tp and t0 are also in BJD.
     """
-    om = np.radians((om + Omega)%360)
+    om = np.radians((om)%360)
+    ma = np.radians(ma)
     f = np.pi/2.0 - om 
     E = 2.0*np.arctan( np.sqrt( (1.0-ecc)/(1.0+ecc) ) * np.tan(f/2.0)  )
-    t_peri    = epoch  - ((ma/TAU)*per)
-    t_transit = t_peri + (E + ecc*np.sin(E)) * (per/TAU)
+    t_peri    =  epoch  - (per/TAU)*(E - ecc*np.sin(E))
+
+    t_transit = t_peri + (E - ecc*np.sin(E)) * (per/TAU)
 
     return t_peri, t_transit
 
 
-def ma_from_t0(per, ecc, om, Omega, t_transit, epoch):
+def get_m0(per, ecc, om):
     '''
     '''
-    om = np.radians((om + Omega)%360)
+    om = np.radians(om)
     f = np.pi/2.0 - om 
     E = 2.0*np.arctan( np.sqrt( (1.0-ecc)/(1.0+ecc) ) * np.tan(f/2.0)  )
-   # t_transit = epoch  - ((ma/TAU)*per) + (E + ecc*np.sin(E)) * (per/TAU)
 
-   # ma =  ((epoch  - t_transit + (E + ecc*np.sin(E)) * (per/TAU))*TAU)/per
-    ma = E + ecc*np.sin(E)
+    ma = E - ecc*np.sin(E)    
     ma = np.degrees(ma)%360.0
 
-
     return ma
+
+
+
 
 def ma_from_epoch(per, t_peri, epoch):
     '''
@@ -2803,3 +2802,34 @@ pl.in
 
 
     return obj
+
+
+
+
+##################### JUNK ########################
+    
+#def ma_from_t0(per, ecc, om, t_transit, epoch):
+#    '''
+#    '''
+#    om = np.radians(om)
+#    f = np.pi/2.0 - om 
+#    E = 2.0*np.arctan( np.sqrt( (1.0-ecc)/(1.0+ecc) ) * np.tan(f/2.0)  )
+#   # t_transit = epoch  - ((ma/TAU)*per) + (E + ecc*np.sin(E)) * (per/TAU)
+
+#    #ma =  ((epoch  - t_transit + (E + ecc*np.sin(E)) * (per/TAU))*TAU)/per
+#    ma = E - ecc*np.sin(E)    
+#    ma = np.degrees(ma)%360.0
+
+
+#    return ma
+
+
+
+
+
+
+
+
+
+
+