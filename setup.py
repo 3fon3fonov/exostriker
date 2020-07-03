f9d9a2646f239b8c6fb4f7d7dc993de488bf3211
Trifon <3fon3fonov@gmail.com>
2020-07-03 21:26:16 +0200

    This is version 0.34

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
 name='exostriker',  
-version='0.33',
+version='0.34',
 scripts=['scripts/exostriker'],
 author="Trifon Trifonov",
 author_email="trifonov@mpia.de",
