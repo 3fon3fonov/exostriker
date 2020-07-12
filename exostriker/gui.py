3dbbaa3951f383b0d7a63f361f9d416903834ed5
Trifon <3fon3fonov@gmail.com>
2020-07-12 11:11:00 +0200

    This is version 0.35

--- exostriker/gui.py
+++ exostriker/gui.py
@@ -5211,7 +5211,7 @@ in https://github.com/3fon3fonov/exostriker
         text = ''
         self.dialog_credits.text.setText(text) 
         
-        text = "You are using 'The Exo-Striker' (ver. 0.34) \n developed by Trifon Trifonov"
+        text = "You are using 'The Exo-Striker' (ver. 0.35) \n developed by Trifon Trifonov"
         
         self.dialog_credits.text.append(text)
 
@@ -8547,7 +8547,7 @@ https://github.com/3fon3fonov/exostriker/issues
         
 
 
-        print("""Hi there! You are running a demo version of the Exo-Striker (ver. 0.34). 
+        print("""Hi there! You are running a demo version of the Exo-Striker (ver. 0.35). 
               
 This version is almost full, but there are still some parts of the tool, which are in a 'Work in progress' state. Please, 'git pull' regularly to be up to date with the newest version.
 """)
