2b9c901c715111965de53cecafd3bc10491e049e
Trifon <3fon3fonov@gmail.com>
2020-08-15 00:00:29 +0200

    GP priors are now correctly loaded from saved sessions (ver. 0.37)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
 name='exostriker',  
-version='0.36',
+version='0.37',
 scripts=['scripts/exostriker'],
 author="Trifon Trifonov",
 author_email="trifonov@mpia.de",
