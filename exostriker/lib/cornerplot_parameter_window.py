560c66b7e5857534b250329d804cc7469a393ddf
Trifon Trifonov <trifonov@mpia.de>
2020-09-21 21:51:20 +0200

    improved cornerplot widget

--- exostriker/lib/cornerplot_parameter_window.py
+++ exostriker/lib/cornerplot_parameter_window.py
@@ -34,6 +34,7 @@ class show_param_boxes(QtWidgets.QDialog):
         
         self.radio_group =QtGui.QButtonGroup(self.widget) # Number group
         self.radio_group2=QtGui.QButtonGroup(self.widget) # Number group
+        self.radio_group3=QtGui.QButtonGroup(self.widget) # Number group
         
         
         self.radio_median     = QtWidgets.QRadioButton('median', self)
@@ -42,6 +43,9 @@ class show_param_boxes(QtWidgets.QDialog):
         self.radio_best_samp  = QtWidgets.QRadioButton('max lnL sample', self)
         self.radio_best_gui   = QtWidgets.QRadioButton('max lnL GUI', self) 
         self.radio_no_cross   = QtWidgets.QRadioButton('do not show', self) 
+        
+        #self.radio_mass_j     = QtWidgets.QRadioButton('Jupiter mass', self)
+       # self.radio_mass_e       = QtWidgets.QRadioButton('Earth mass', self)        
         
         self.label_bestfit    = QtWidgets.QLabel("Truths as:")
         
@@ -52,10 +56,15 @@ class show_param_boxes(QtWidgets.QDialog):
         self.radio_group.addButton(self.radio_best_gui,4)
         self.radio_group.addButton(self.radio_no_cross,5)
 
-     #   self.radio_mp_So      = QtWidgets.QRadioButton('Solar', self)
-     #   self.radio_mp_Mj      = QtWidgets.QRadioButton('Jupiter', self)
-     #   self.radio_mp_Me      = QtWidgets.QRadioButton('Earth', self)        
+        self.radio_mp_So      = QtWidgets.QRadioButton('Solar', self)
+        self.radio_mp_Mj      = QtWidgets.QRadioButton('Jupiter', self)
+        self.radio_mp_Me      = QtWidgets.QRadioButton('Earth', self)        
  
+    
+        self.radio_group3.addButton(self.radio_mp_So,0)
+        self.radio_group3.addButton(self.radio_mp_Mj,1)
+        self.radio_group3.addButton(self.radio_mp_Me,2)
+    
         self.mass_check  = QtWidgets.QCheckBox('incl. mass (needed: K,P,e )', self)
         self.semi_check  = QtWidgets.QCheckBox('incl. semi-major axis (needed: P)', self)
         self.radi_check  = QtWidgets.QCheckBox('incl. radius (needed: Rp/Rs)', self)
@@ -64,10 +73,12 @@ class show_param_boxes(QtWidgets.QDialog):
         
        # self.mass_check.setChecked(bool(self.parent.lables_cornerplot['mass']))
 
-        self.radio_bestfit_labels = ["median","mean","mode","best_samp","best_gui","none","mass","semimajor","radius"]
+        self.radio_bestfit_labels = ["median","mean","mode","best_samp","best_gui",
+                                     "none","mass","use_Me","use_Mj","use_Ms","semimajor","radius"]
         self.radio_group_list = [self.radio_median,self.radio_mean,self.radio_mode,
                                  self.radio_best_samp,self.radio_best_gui,self.radio_no_cross,
-                                 self.mass_check,self.semi_check,self.radi_check]
+                                 self.mass_check,self.radio_mp_Me, self.radio_mp_Mj, self.radio_mp_So, 
+                                 self.semi_check,self.radi_check]
         
         #self.radio_group_mass_list = [self.radio_mp_So,self.radio_mp_Mj,self.radio_mp_Me,
        #                          self.radio_best_samp,self.radio_best_gui,self.radio_no_cross,
@@ -80,7 +91,6 @@ class show_param_boxes(QtWidgets.QDialog):
         self.cancel_button = QtGui.QPushButton('Close', self)
         
    
-
         self.truth_color_button = QtGui.QPushButton('Truths color', self)
         self.truth_color_button.clicked.connect(self.get_color_truth)
 
@@ -112,6 +122,18 @@ class show_param_boxes(QtWidgets.QDialog):
             self.mass_check.setChecked(self.parent.lables_cornerplot["mass"])
             self.semi_check.setChecked(self.parent.lables_cornerplot["semimajor"])
             self.radi_check.setChecked(self.parent.lables_cornerplot["radius"])
+            
+            try:
+                self.radio_mp_So.setChecked(self.parent.lables_cornerplot["use_Ms"])
+                self.radio_mp_Mj.setChecked(self.parent.lables_cornerplot["use_Mj"])
+                self.radio_mp_Me.setChecked(self.parent.lables_cornerplot["use_Me"])       
+            except:
+                self.parent.lables_cornerplot["use_Ms"] = False
+                self.parent.lables_cornerplot["use_Mj"] = False
+                self.parent.lables_cornerplot["use_Me"] = True
+                self.radio_mp_So.setChecked(self.parent.lables_cornerplot["use_Ms"])
+                self.radio_mp_Mj.setChecked(self.parent.lables_cornerplot["use_Mj"])
+                self.radio_mp_Me.setChecked(self.parent.lables_cornerplot["use_Me"])              
             
             self.cornerplot_opt2 = {"bins":25,
                               "reverse":True,
@@ -158,7 +180,7 @@ class show_param_boxes(QtWidgets.QDialog):
         
         k = 0
         l = 0
-        for g in range(len(self.parent.lables_cornerplot)-11):
+        for g in range(len(self.parent.lables_cornerplot)-14):
 
             k = g%20
 
@@ -185,12 +207,14 @@ class show_param_boxes(QtWidgets.QDialog):
             self.text_boxes.append(self.text_panel) 
 #            k = g
             #for 
-            
-            
+ 
         #for g in range(4):
         self.layout.addWidget(self.mass_check, 0, l+2)
-        self.layout.addWidget(self.semi_check, 1, l+2)
-        self.layout.addWidget(self.radi_check, 2, l+2)
+        self.layout.addWidget(self.radio_mp_Me, 1,  l+2)
+        self.layout.addWidget(self.radio_mp_Mj, 2,  l+2)         
+        self.layout.addWidget(self.radio_mp_So, 3, l+2)        
+        self.layout.addWidget(self.semi_check, 4, l+2)
+        self.layout.addWidget(self.radi_check, 5, l+2)
 
          
         self.layout.addWidget(self.label_bestfit, 0,l+3)              
@@ -243,7 +267,7 @@ class show_param_boxes(QtWidgets.QDialog):
             
         results = {k: np.array([self.text_boxes[k].toPlainText(), self.checked_boxes[k].isChecked()]) for k in range(len(self.checked_boxes))}
         
-        for k in range(9):
+        for k in range(12):
             #print(self.radio_bestfit_labels[k],self.radio_group_list[k].isChecked() )
             results[self.radio_bestfit_labels[k]] = self.radio_group_list[k].isChecked() 
         
