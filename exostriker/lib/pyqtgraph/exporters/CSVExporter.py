3b13b02023a36ab0451fda1e18e8790c952c874b
Trifon Trifonov <trifonov@mpia.de>
2021-05-27 20:29:30 +0200

    .csv exporter of plots now can export y- and x-axis errorbars

--- exostriker/lib/pyqtgraph/exporters/CSVExporter.py
+++ exostriker/lib/pyqtgraph/exporters/CSVExporter.py
@@ -16,7 +16,7 @@ class CSVExporter(Exporter):
         self.params = Parameter(name='params', type='group', children=[
             {'name': 'separator', 'type': 'list', 'value': 'comma', 'values': ['comma', 'tab']},
             {'name': 'precision', 'type': 'int', 'value': 10, 'limits': [0, None]},
-            {'name': 'columnMode', 'type': 'list', 'values': ['(x,y) per plot', '(x,y,y,y) for all plots']}
+            {'name': 'columnMode', 'type': 'list', 'values': ['(x,y,err_y,err_x) per plot', '(x,y,y,y) for all plots']}
         ])
         
     def parameters(self):
@@ -34,23 +34,51 @@ class CSVExporter(Exporter):
         data = []
         header = []
 
-        appendAllX = self.params['columnMode'] == '(x,y) per plot'
+        appendAllX = self.params['columnMode'] == '(x,y,err_y,err_x) per plot'
 
         for i, c in enumerate(self.item.curves):
             cd = c.getData()
             if cd[0] is None:
                 continue
             data.append(cd)
+
+            #print(cd)
+
+
             if hasattr(c, 'implements') and c.implements('plotData') and c.name() is not None:
                 name = c.name().replace('"', '""') + '_'
                 xName, yName = '"'+name+'x"', '"'+name+'y"'
             else:
                 xName = 'x%04d' % i
                 yName = 'y%04d' % i
+                e_yName = 'e_y%04d' % i
+                e_xName = 'e_x%04d' % i
+
             if appendAllX or i == 0:
-                header.extend([xName, yName])
+                header.extend([xName, yName,e_yName,e_xName ])
             else:
-                header.extend([yName])
+                header.extend([yName,e_yName,e_xName ])
+
+        for indx, item in enumerate(self.item.items):
+
+            if self.item.items[indx].__class__.__name__ == "InfiniteLine":                   
+                continue   
+            if self.item.items[indx].__class__.__name__ == "FillBetweenItem":                               
+                continue
+            if self.item.items[indx].__class__.__name__ == "TextItem":
+                continue
+
+            if "top" in self.item.items[indx].opts:
+                yerr=self.item.items[indx].opts["top"]
+                data[1] = list(data[1])
+                data[1].append(yerr)
+                data[1] = tuple(data[1])
+
+            if "left" in self.item.items[indx].opts:
+                xerr=self.item.items[indx].opts["left"]
+                data[1] = list(data[1])
+                data[1].append(xerr)
+                data[1] = tuple(data[1])
 
         if self.params['separator'] == 'comma':
             sep = ','
@@ -77,6 +105,19 @@ class CSVExporter(Exporter):
                         fd.write(numFormat % d[1][i] + sep)
                     else:
                         fd.write(' %s' % sep)
+
+                    # write data e_y value
+                    if d is not None and len(d)>2 and d[2] is not None and i < len(d[2]):
+                            fd.write(numFormat % d[2][i] + sep)
+                    else:
+                        fd.write(' %s' % sep)
+
+                    # write data e_x value
+                    if d is not None and len(d)>3 and d[3] is not None and i < len(d[3]):
+                            fd.write(numFormat % d[3][i] + sep)
+                    else:
+                        fd.write(' %s' % sep)
+
                 fd.write('\n')
 
 
