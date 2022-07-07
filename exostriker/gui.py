f05c0c9c99ddedb5ee36b3e4c5c7d17aa44e93b6
Trifon Trifonov <trifonov@mpia.de>
2022-07-07 12:31:10 +0200

    P priors spinbox precision increased

--- exostriker/gui.py
+++ exostriker/gui.py
@@ -132,9 +132,9 @@ os.environ["QT_SCREEN_SCALE_FACTORS"] = "1"
 os.environ["QT_SCALE_FACTOR"] = "1"
 os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "0"
 os.environ["QT_DEVICE_PIXEL_RATIO"] = "1"
+#os.environ["QT_LOGGING_RULES"] = '*.debug=false'
+#os.system("export QT_LOGGING_RULES")
 
- 
- 
 #if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
 #    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
 
