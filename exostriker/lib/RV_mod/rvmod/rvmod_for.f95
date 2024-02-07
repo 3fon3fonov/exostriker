7a0cefbf6c5ec6dedbb0ff528c3161479676a0e4
Trifon Trifonov <trifonov@mpia.de>
2024-02-07 19:48:27 +0100

    macOS fortran f2py test

--- exostriker/lib/RV_mod/rvmod/rvmod_for.f95
+++ exostriker/lib/RV_mod/rvmod/rvmod_for.f95
@@ -720,7 +720,7 @@ subroutine dynfit_lm(epsil, deltat, amoebastarts, &
 !f2py intent(out) bestpar_1, bestpar_2, bestpar_3, bestpar_4
 !f2py depend(ndset_in) files_array,files_param
 !f2py depend(npl_in) array_npl, dynamical_planets
-!f2py depend(ndata) data_array,fit_array
+!f2py depend(ndata) data_array
 
     common /DSBLK/ npl, ndset, idsmax, idset, gr_flag
     common mstar, sini
@@ -1997,7 +1997,7 @@ subroutine io_write_bf_ewcop_fin_dynlm (a, covar, t, ys, ndata, ts, &
     real(4) :: model_max, model_min
     parameter (AU = 1.49597892d11, day = 86400.d0)
     real(8) :: wdot(NPLMAX), u_wdot(NPLMAX), best_w, best_we
-    real(8) :: ymod_pl(npl, 20000)     
+    real(8) :: ymod_pl(npl, ndata),ymod_pl2(npl,nt)    
     real(8) :: res_array(ndata, 6 + npl)
     real(8) :: fit_return(4), fit_array(nt, 2 + npl)
     real(8) :: bestpar_1(npl, 17, 2), bestpar_2(ndset, 2)
@@ -2149,7 +2149,7 @@ subroutine io_write_bf_ewcop_fin_dynlm (a, covar, t, ys, ndata, ts, &
         do i = 1, nt
             x(i) = (i - 1) * dt * 8.64d4
         enddo
-        call RVKEP_ewcop_fin (x, a, ymod, ymod_pl, dyda, ma, nt, epsil, deltat,&
+        call RVKEP_ewcop_fin (x, a, ymod, ymod_pl2, dyda, ma, nt, epsil, deltat,&
          hkl, coplar_inc)
         do i = 1, nt
 !            write(*,*) i,ymod(i), a(7 * npl * ndset + 1), & 
@@ -2158,7 +2158,7 @@ subroutine io_write_bf_ewcop_fin_dynlm (a, covar, t, ys, ndata, ts, &
             fit_array(i, :) = (/ t0 + x(i) / 8.64d4, &
                     ymod(i) + a(7 * npl + ndset + 1) * (x(i) / 8.64d4)&
                             + a(7 * npl + ndset + 2) * (x(i) / 8.64d4)**2, &
-                            (ymod_pl(j,i), j=1,npl)/)
+                            (ymod_pl2(j,i), j=1,npl)/)
 
         enddo
     endif
